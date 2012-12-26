6a4905e7ea70e06b8860a4607786573a913d5ede
Rok Garbas <rok@garbas.si>
2012-12-26 08:40:49 +0100

    first working example

---
+++ Makefile
@@ -0,0 +1,32 @@
+LESSC = lessc
+NODEJS = node
+STANDALONE = name=lib/almond include=main wrap=true
+TARGETS = build/css/frontend.css build/css/backend.css build/js/backend.js build/js/frontend.js build/images build/index.html
+
+all:: clean $(TARGETS)
+
+build/css/backend.css:
+	$(LESSC) src/less/backend.less -x $@
+build/css/frontend.css:
+	$(LESSC) src/less/frontend.less -x $@
+build/js/frontend.js:
+	$(NODEJS) lib/r.js -o baseUrl=src insertRequire=frontend mainConfigFile=src/frontend.js name=../lib/almond include=frontend wrap=true optimize=uglify out=$@
+build/js/backend.js:
+	$(NODEJS) lib/r.js -o baseUrl=src insertRequire=backend mainConfigFile=src/backend.js name=../lib/almond include=backend wrap=true optimize=uglify out=$@
+build/images:
+	mkdir -p build/images
+	cp images/* build/images
+build/index.html:
+	cp index.html $@
+	sed -i 's@stylesheet/less@stylesheet@g' $@
+	sed -i 's@src/less/frontend.less@css/frontend.css@g' $@
+	sed -i 's@lib/less.js"></script><script data-main="src/frontend" src="lib/require.js@js/frontend.js@g' $@
+	sed -i 's@data-iframe-resources="src/less/backend.less;lib/less.js;lib/require.js?data-main=src/backend;"@data-iframe-resources="css/backend.css;js/backend.js"@g' $@
+
+bootstrap:
+	git submodule update --init --recursive
+
+clean:
+	rm -rf $(TARGETS)
+
+.PHONY: all clean bootstrap test
--- js/map.js
+++ js/map.js
@@ -11,18 +11,34 @@ define([
 
       //$.jHERE.defaultCredentials(appId, authToken);
 
+      $el.css({
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        overflow: 'hidden'
+      });
+
+      this.resize();
+      $el.on('resize', this.resize);
+
       $el.jHERE({
         enable: [
-          'behavior', // enables map interaction (drag to pan, scroll-wheel to zoom)
-          'zoombar', // shows a zoom control
-          'scalebar' // shows a scalebar on the map
+        'behavior', // enables map interaction (drag to pan, scroll-wheel to zoom)
+        'zoombar', // shows a zoom control
+        'scalebar', // shows a scalebar on the map
+        'typeselector', // shows a dropdown where the user can select map, satellite, terrain
+        'overview', // shows a button to activate the overview panel
+        //'traffic', // shows a button to enable the traffic layer
+        //'publictransport', // shows a button to enable the public transport view
+        //'positioning', // shows a button that triggers detection of the user's position
+        //'rightclick', // shows a contextual menu on right click to zoom in and out
+        'contextmenu' // shows an enriched contextual menu with: current address, zoom in/out, directions
+
         ], // An array of components as strings.
-        type: 'smart', //can be map (the default), satellite, terrain, smart, pt.
         zoom: 15, //a positive integer
         center: [ parseFloat(this.options.home_latitude),
                   parseFloat(this.options.home_longitude) ] // center map into center of ljubljana
       });
-
 
       $.each(window.LPP_STATIONS, function(i, station) {
         $el.jHERE('marker',
@@ -35,6 +51,12 @@ define([
               icon: 'images/marker.png',
               anchor: {x: 12, y: 32}
             });
+      });
+    },
+    resize: function() {
+      this.$el.css({
+        width: $(window).width(),
+        height: $(window).height()
       });
     },
     center: function(lat, lon) {
